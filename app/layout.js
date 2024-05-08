30af4765e16a17e2704efce6f01ed045e4315ec4
Travis <traviscmiller01@gmail.com>
2024-05-07 18:20:28 -0700

    changed default font to balthazar, tested tailwind @layer css functions

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,19 @@
-import { Inter } from "next/font/google";
+import { Balthazar } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const balthazar = Balthazar({ subsets: ["latin"], weight: '400' });
 
 export const metadata = {
-  title: "Campaign Companion",
-  description: "A tool to help a GM or DM organize their TTRPG campaign",
+	title: "Campaign Companion",
+	description: "A tool to help a GM or DM organize their TTRPG campaign",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={`${inter.className} h-screen`}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`${balthazar.className} w-full h-full`}>
+				{children}
+			</body>
+		</html>
+	);
 }
